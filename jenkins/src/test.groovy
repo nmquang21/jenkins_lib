8bee7e7e9bf3dd6c7b2005b0e7fac864a9fcf234
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-18 17:24:15 +0700

    Create test.groovy

---
+++ jenkins/src/test.groovy
@@ -0,0 +1,39 @@
+def call(){
+pipeline {
+    agent {
+        node {
+            label 'ssh-agent-node-01'
+        }
+    }
+    
+    stages {
+       
+        
+        stage('Build') {
+            steps {
+                sh 'docker --version'
+                sh 'docker ps'
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                echo 'mvn test'
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo 'mvn deploy'
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+}
