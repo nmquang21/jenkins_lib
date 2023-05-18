58735756e4a4d3a237f1ba140ced5feb1c9fa7f4
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-18 17:07:52 +0700

    Update test.groovy

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -1,1 +1,36 @@
-test
+pipeline {
+    agent {
+        node {
+            label 'ssh-agent-node-01'
+        }
+    }
+    
+    stages {
+       
+        
+        stage('Build') {
+            steps {
+                sh 'docker --version'
+                sh 'docker ps'
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                echo 'mvn test'
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo 'mvn deploy'
+            }
+        }
+    }
+}
+
+
+
+
+
+
