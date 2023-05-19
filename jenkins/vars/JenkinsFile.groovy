d4a555b7243d199cde27cfca3237d0dd8d05784f
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-19 10:37:02 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -13,6 +13,10 @@ pipeline {
             steps {
                 sh 'docker --version'
                 sh 'docker ps'
+                sshagent(['cfc80002-d34b-4b12-b640-fe12232c79f1']) {
+                    // some block
+                    sh 'ssh nmquang21@35.234.5.238 "touch /home/nmquang21/file_demo_ssh.txt"'
+                }
             }
         }
         
