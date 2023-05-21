fcc052fa2584534e0b1a2fe8a0d4d267640098aa
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-21 16:43:42 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -22,8 +22,9 @@ pipeline {
                 sh 'docker --version'
                 sh 'docker ps'
                 sshagent(credentials:['b1fd8109-9b99-4fd2-8db7-5a898625b64e']) {
-                    // some block
+                    // some bloc kmkdir 
                     sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/jenkins_test:V1'
+                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker mkdir booking_hihi'
                 }
             }
         }
