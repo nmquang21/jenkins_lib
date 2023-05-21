7978d2992a87877a7d1c3882468506193e68aa43
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-21 17:17:29 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -24,16 +24,7 @@ pipeline {
                 sshagent(credentials:['b1fd8109-9b99-4fd2-8db7-5a898625b64e']) {
                     // some bloc kmkdir 
                     sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/jenkins_test:V1'
-                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 "mkdir -p booking_hihi; touch docker-compose.yml && echo "version: '3.7'
-
-services:
-  app:
-    build:
-      context: .
-      image: nmquang21/jenkins:V1
-    ports:
-      - '5000:80'
-    restart: unless-stopped" > docker-compose.yml"'
+                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 "mkdir -p booking_hihi'
                     sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 pwd'
                 }
             }
