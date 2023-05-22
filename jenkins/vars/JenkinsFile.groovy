1f1d83334ceb09600c233beb75e7c87242721f23
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-22 15:05:55 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -1,47 +1,61 @@
-def call(){
 pipeline {
-    agent {
+    //agent any
+    agent{
         node {
-            label 'ssh-agent-node-01'
-        }
+                   label 'ssh-agent-node-01'
+         }
+     }
+   
+    parameters {
+        string(name: 'VERSION', defaultValue: 'latest', description: 'Phien ban build?')
     }
     environment {
-    /*
-     define your command in variable
-     */
-    remoteCommands =
-      """java --version;
-    java --version;
-    java --version """
-  }
+        // Định nghĩa global
+        BRANCH_NAME = 'masters'
+    }
     stages {
-       
-        
+        stage('Clone code') {
+            steps {
+                git credentialsId: '1fd902f5-1ee3-4b89-b907-834346b62625', url: 'https://github.com/nmquang21/RoomBookingUI.git'
+            }
+        }
+        stage('Build Image') {
+            steps {           
+                sh 'npm install'
+                sh 'npm run build'               
+            }
+        }
+        stage('Push Image Docker hub') {
+            steps {
+                withDockerRegistry(credentialsId: 'docker_hub', url: 'https://index.docker.io/v1/') {
+                    sh 'docker build -t nmquang21/jenkins_test:${VERSION} .'
+                    sh 'docker push nmquang21/jenkins_test:${VERSION}'
+                    sh 'docker rmi nmquang21/jenkins_test:${VERSION}'
+                }
+            }
+        }
         stage('Build') {
             steps {
                 sh 'docker --version'
                 sh 'docker ps'
                 sshagent(credentials:['b1fd8109-9b99-4fd2-8db7-5a898625b64e']) {
                     // some bloc kmkdir 
-                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/jenkins_test:V1'
-                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 "mkdir -p booking_hihi'
-                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 pwd'
+                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/jenkins_test:${VERSION}'
+                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker rm room_hihi --force'
+                    sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker run -d --name room_hihi -p 8880:80 nmquang21/jenkins_test:${VERSION}'
                 }
             }
         }
-        
-        stage('Test') {
+        stage('Example') {
             steps {
-                echo 'mvn test'
-            }
-        }
-        
-        stage('Deploy') {
-            steps {
-                echo 'mvn deploy'
-                echo 'mvn deployyy'
+                script {
+                    if (env.BRANCH_NAME == 'master') {
+                        echo 'I only execute on the master branch'
+                    } else {
+                        echo 'I execute elsewhere'
+                    }
+                }
             }
         }
     }
-}
 }
