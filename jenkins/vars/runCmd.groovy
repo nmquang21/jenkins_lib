e8be46e8e6ea8425ea234dae467281da6acc6df4
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-22 16:00:42 +0700

    Create runCmd.groovy

---
+++ jenkins/vars/runCmd.groovy
@@ -0,0 +1,14 @@
+def call(String commands){
+    def returnCode = 0
+    try{
+        if(isUnix()){
+            returnCode = sh(script: "${commands}", returnStatus: true)
+        }
+        else{
+            returnCode = bat(script: "${commands}", returnStatus: true)
+        }
+    }catch(ex){
+        return code = -1
+    }
+    return returnCode == 0
+}
