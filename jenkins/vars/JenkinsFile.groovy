d31b1e06909e321a6faf643c5b9b999a9141578a
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-22 15:53:43 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -25,7 +25,7 @@ def call(){
                  //   sh 'npm install'
                 //    sh 'npm run build'               
                // }
-                script{
+                step{
                     BuildImage()
                 }
                 
