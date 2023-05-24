1d2c1e942231cfbce5acc976f62e6460afff2bf4
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-24 22:06:54 +0700

    Update publishTest.groovy

--- jenkins/vars/publishTest.groovy
+++ jenkins/vars/publishTest.groovy
@@ -1,8 +1,21 @@
 def call(){
    BRANCH_NAME = 'master'
    BUILD_VERSION = '1.0.0'
+   def tasks = []
+
+   tasks.add({
+       // Tác vụ 1
+       echo "Task 1 executed"
+   })
+
+   tasks.add({
+       // Tác vụ 2
+       echo "Task 2 executed"
+   })
     node('ssh-agent-node-01') {
-        
+        stage('Parallel Stage') {
+           parallel tasks
+       }
         stage('Clone') {
             echo "Branch name: ${BRANCH_NAME}"
         }
