b056c8f5c840e0f8c0558b47afc992e1af62aecb
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-24 21:35:05 +0700

    Update publishTest.groovy

--- jenkins/vars/publishTest.groovy
+++ jenkins/vars/publishTest.groovy
@@ -11,14 +11,10 @@ def call(){
             echo 'Build'
         }
         stage('Example') {
-            steps {
-                script {
-                    if (env.BRANCH_NAME == 'master') {
-                        echo 'I only execute on the master branch'
-                    } else {
-                        echo 'I execute elsewhere'
-                    }
-                }
+            if (env.BRANCH_NAME == 'master') {
+                echo 'I only execute on the master branch'
+            } else {
+                echo 'I execute elsewhere'
             }
         }
     }
