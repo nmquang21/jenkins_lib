c2d0c0a7a8403af05c7252cc4f30a705c231da56
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-25 20:35:36 +0700

    Update JenkinsFile.groovy

--- jenkins/vars/JenkinsFile.groovy
+++ jenkins/vars/JenkinsFile.groovy
@@ -1,67 +1,130 @@
-def call(){
-    pipeline {
-        //agent any
-        agent{
-            node {
-                       label 'ssh-agent-node-01'
-             }
-         }
+//import Contants
 
-        parameters {
-            string(name: 'VERSION', defaultValue: 'latest', description: 'Phien ban build?')
-        }
-        environment {
-            // Định nghĩa global
-            BRANCH_NAME = 'masters'
-        }
-        stages {
-            stage('Clone code') {
-                steps {
-                    git credentialsId: '1fd902f5-1ee3-4b89-b907-834346b62625', url: 'https://github.com/nmquang21/RoomBookingUI.git'
-                }
+def call(){
+    STRING_DELIMITER = ','
+    BACKEND_GIT_URL = 'https://github.com/PhamTam2k1/RoomBooking.git'
+    FRONTEND_GIT_URL = 'https://github.com/PhamTam2k1/RoomBookingUniversityUI.git'
+
+    BACKEND_WORKSPACE = ''
+    FRONTEND_WORKSPACE = ''
+
+    DOTNET_BASEIMAGE = 'aspnet:6.0.9'
+    VUE_BASEIMAGE = 'nginx:1.23.1-alpine'
+
+    PROJS = [:]
+    PROJS['app/api'] = 'MISA.API'
+    PROJS['app/ui'] = 'MISA.UI'
+
+    pipeline{
+        agent{
+            node{
+                label 'ssh-agent-node-01'
             }
-            stage('Build Image') {
-                //steps {           
-                 //   sh 'npm install'
-                //    sh 'npm run build'               
-               // }
+        }
+        //options{
+        //  disableConcurentBuilds()
+            //buildDiscarder()
+        // }
+        parameters{
+            extendedChoice( 
+                name: 'APP_BUILD', 
+                value: 'APP/FRONTEND,APP/BACKEND', 
+                multiSelectDelimiter: STRING_DELIMITER, 
+                quoteValue: false,
+                saveJSONParameterToFile: false,
+                type: 'PT_CHECKBOX', 
+                description: 'Chon app build?', 
+            )
+            string(
+                defaultValue: 'master',
+                name: 'FRONTEND_GIT_BRANCH',
+                trim: true,
+                description: 'Build Front End nhánh nào?',
+            )
+            string(
+                defaultValue: 'master',
+                name: 'BACKEND_GIT_BRANCH',
+                trim: true,
+                description: 'Build Back End nhánh nào?',
+            )
+        }
+        stages{
+            stage('get lastet source'){
                 steps{
-                    BuildImage()
-                }
-                
-            }
-            stage('Push Image Docker hub') {
-                steps {
-                    withDockerRegistry(credentialsId: 'docker_hub', url: 'https://index.docker.io/v1/') {
-                        sh 'docker build -t nmquang21/jenkins_test:${VERSION} .'
-                        sh 'docker push nmquang21/jenkins_test:${VERSION}'
-                        sh 'docker rmi nmquang21/jenkins_test:${VERSION}'
+                    script{
+                        getSourceTasks = [:]
+                        selectedAppBuild = params.APP_BUILD.split(STRING_DELIMITER)
+                        if(params.APP_BUILD != '' && params.APP_BUILD != STRING_DELIMITER && selectedAppBuild.size() > 0){
+                            selectedAppBuild.each{app ->
+                                if(app == 'APP/FRONTEND'){
+                                    getSourceTasks[app] = {
+                                        echo pwd()
+                                        dir(BACKEND_WORKSPACE){
+                                            script{
+                                                checkout(
+                                                    [
+                                                    $class: 'GitSCM',
+                                                    branch: [[name: params.FRONTEND_GIT_BRANCH]], 
+                                                    userRemoteConfigs:
+                                                        [
+                                                            [
+                                                                credentialsId: '1fd902f5-1ee3-4b89-b907-834346b62625',
+                                                                url: FRONTEND_GIT_URL
+                                                            ]
+                                                        ] 
+                                                    ]
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                                if(app == 'APP/BACKEND'){
+                                    getSourceTasks['FrontEnd'] = {
+                                        echo pwd()
+                                        dir(FRONTEND_WORKSPACE){
+                                            script{
+                                                checkout(
+                                                    [
+                                                    $class: 'GitSCM',
+                                                    branch: [[name: params.BACKEND_GIT_BRANCH]], 
+                                                    userRemoteConfigs:
+                                                        [
+                                                            [
+                                                                credentialsId: '1fd902f5-1ee3-4b89-b907-834346b62625',
+                                                                url: BACKEND_GIT_URL
+                                                            ]
+                                                        ] 
+                                                    ]
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        parallel getSourceTasks
                     }
                 }
             }
-            stage('Build') {
-                steps {
-                    sh 'docker --version'
-                    sh 'docker ps'
-                    sshagent(credentials:['b1fd8109-9b99-4fd2-8db7-5a898625b64e']) {
-                        // some bloc kmkdir 
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/jenkins_test:${VERSION}'
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker rm room_hihi --force'
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker run -d --name room_hihi -p 8880:80 nmquang21/jenkins_test:${VERSION}'
-                    }
-                }
-            }
-            stage('Example') {
-                steps {
-                    script {
-                        if (env.BRANCH_NAME == 'master') {
-                            echo 'I only execute on the master branch'
-                        } else {
-                            echo 'I execute elsewhere'
+            stage('publish'){
+                steps{
+                    script{
+                        taskPublish = [:]
+                        selectedAppBuild = params.APP_BUILD.split(STRING_DELIMITER)
+                        if(params.APP_BUILD != '' && params.APP_BUILD != STRING_DELIMITER && selectedAppBuild.size() > 0){
+                            selectedAppBuild.each{app ->
+                                if(app == 'APP/FRONTEND'){
+                                    echo 'Puplish APP/FRONTEND'
+                                }
+                                if(app == 'APP/BACKEND'){
+                                    echo 'Puplish APP/BACKEND'
+                                }
+                            }
+                            parallel taskPublish
                         }
                     }
                 }
             }
-        }
+        } 
     }
 }
