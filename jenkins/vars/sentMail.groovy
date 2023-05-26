294dbb5ff7db01211fa9f18144346896f5fc3640
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-26 14:50:18 +0700

    Update sentMail.groovy

--- jenkins/vars/sentMail.groovy
+++ jenkins/vars/sentMail.groovy
@@ -4,7 +4,10 @@ def call(String version){
         def textVersion = '1.0'
         def emailBody = """
             <html>
-                <strong>demo mail</strong>
+                <strong>Đã có bản build mới!</strong>
+                <div>Truy cập tại địa chỉ <a href="utc-room.online">utc-room.online</a></div>
+                <div>Tài khoản: <strong>phamtamhg01@gmail.com</strong></div>
+                <div>Mật khẩu: <strong>12345678@Utc</strong></div>
             </html>
         """
         emailext body : emailBody,
