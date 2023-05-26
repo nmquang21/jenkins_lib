db5081864d44648f83b49dd70ba69ff92cf401ca
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-26 14:15:54 +0700

    Update publishRoomBookingUniversity.groovy

--- jenkins/vars/publishRoomBookingUniversity.groovy
+++ jenkins/vars/publishRoomBookingUniversity.groovy
@@ -29,7 +29,7 @@ def call(){
         }
         post{
             always{
-                mail bcc: '', emailext body: '<div>build thành công, truy cập bản build tại <a href="utc-room.online">utc-room.online</a></div>',  cc: '',from: '', replyTo: '', subject: 'Thông báo bản build utc-room.online',to: 'nmquang21@gmail.com,nguyenminhquang_t62@hus.edu.vn'
+                sentMail('1.3')
             }
         }
     }
