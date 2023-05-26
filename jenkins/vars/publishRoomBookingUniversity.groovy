1c04848a41989badc9ef0d04f49adcd55ab94e0c
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-26 11:48:38 +0700

    Update publishRoomBookingUniversity.groovy

--- jenkins/vars/publishRoomBookingUniversity.groovy
+++ jenkins/vars/publishRoomBookingUniversity.groovy
@@ -14,42 +14,7 @@ def call(){
             // Định nghĩa global
             BRANCH_NAME = 'masters'
         }
-        stages {
-            stage('Clone code') {
-                steps {
-                    git credentialsId: '1fd902f5-1ee3-4b89-b907-834346b62625', url: 'https://github.com/PhamTam2k1/RoomBookingUniversityUI.git'
-                }
-            }
-            stage('Build Image') {
-                //steps {           
-                 //   sh 'npm install'
-                //    sh 'npm run build'               
-               // }
-                steps{
-                    BuildImage()
-                }  
-            }
-            stage('Push Image Docker hub') {
-                steps {
-                    withDockerRegistry(credentialsId: 'docker_hub', url: 'https://index.docker.io/v1/') {
-                        sh 'docker build -t nmquang21/room_booking_university:${VERSION} .'
-                        sh 'docker push nmquang21/room_booking_university:${VERSION}'
-                        sh 'docker rmi nmquang21/room_booking_university:${VERSION}'
-                    }
-                }
-            }
-            stage('Build') {
-                steps {
-                    sh 'docker --version'
-                    sh 'docker ps'
-                    sshagent(credentials:['b1fd8109-9b99-4fd2-8db7-5a898625b64e']) {
-                        // some bloc kmkdir 
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker pull nmquang21/room_booking_university:${VERSION}'
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker rm RoomBookingUniversity --force'
-                        sh 'ssh -o StrictHostKeyChecking=no -l root 34.96.176.17 docker run -d --name RoomBookingUniversity -p 80:80 nmquang21/room_booking_university:${VERSION}'
-                    }
-                }
-            }
+        stages {      
             stage('Example') {
                 steps {
                     script {
@@ -60,6 +25,11 @@ def call(){
                         }
                     }
                 }
+            }
+        }
+        post{
+            always{
+                mail bcc: '', body: 'build thành công', cc: 'nguyenminhquang_t62@hus.edu.vn', from: '', replyTo: '', subject: 'Thông báo bản build utc-room.online', to: 'nmquang21@gmail.com'
             }
         }
     }
