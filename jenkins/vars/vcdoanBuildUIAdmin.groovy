589f5d93da22f9abdded7f10e0329a53c334fb20
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-06-03 10:28:31 +0700

    Update vcdoanBuildUIAdmin.groovy

--- jenkins/vars/vcdoanBuildUIAdmin.groovy
+++ jenkins/vars/vcdoanBuildUIAdmin.groovy
@@ -120,6 +120,7 @@ def call(){
                                                         //runCmd('npm i -g @vue/cli')
                                                     }
                                                     def commands = [
+                                                        'whoami'
                                                         'node -v',
                                                         'npm i',
                                                         'npm run build'
