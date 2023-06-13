1961e402bcbc5c3a257dc256ae66530e5e7102f4
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-06-13 15:20:19 +0700

    Update testPipline.groovy

--- jenkins/vars/testPipline.groovy
+++ jenkins/vars/testPipline.groovy
@@ -62,7 +62,7 @@ def call(){
                                                 checkout(
                                                     [
                                                     $class: 'GitSCM',
-                                                    branches: params.FRONTEND_GIT_BRANCH, 
+                                                    branches: [[name: params.FRONTEND_GIT_BRANCH]], 
                                                     userRemoteConfigs:
                                                         [
                                                             [
@@ -84,7 +84,7 @@ def call(){
                                                 checkout(
                                                     [
                                                     $class: 'GitSCM',
-                                                    branches: params.BACKEND_GIT_BRANCH, 
+                                                    branches: [[name: params.BACKEND_GIT_BRANCH]], 
                                                     userRemoteConfigs:
                                                         [
                                                             [
